31c1e8eab4c9a1c4e999dc371f2c97f1ad3edd04
718annie <718annie@gmail.com>
2020-08-25 15:41:38 +0900

    8/25

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button, Image, Divider, Header, Icon, Grid } from "semantic-ui-react";
+
+import Comments from "./comment.js";
+import Buttons from "./buttons.js";
+
+import 포스터 from "./포스터.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style = {{backgroundColor : "black"}}>
+      <Grid centered>
+        <Grid.Row>
+          <Image src={포스터} centered />
+        </Grid.Row>
+        <Grid.Row>
+          <Buttons />
+        </Grid.Row>
+      </Grid>
+      <br />
+
+      <Divider horizontal >
+        <Header as="h4" style = {{color : "white"}}>
+          <Icon name="comment alternate" style = {{color : "white" }}/>
+          댓글을 입력하세요
+        </Header>
+      </Divider>
+
+      <Comments />
+      <Divider horizontal>
+        <Header as="h4"> style = {{color : "white" }}>
+          <Icon name="microchip" style = {{color : "white" }}/>
+          Contact Me
+        </Header>
+      </Divider>
+      <br />
+      <div>
+        <Grid centered>
+          <Button circular color="facebook" icon="facebook" onClick = {()=> alert("준비중입니다.")} />
+          <Button
+            circular
+            color="instagram"
+            icon="instagram"
+            onClick={() => window.open("https://www.instagram.com/?hl=ko")}
+          />
+          <Button
+            circular
+            color="youtube"
+            icon="youtube"
+            onClick={() =>
+              window.open(
+                "https://www.youtube.com/channel/UCxnaK0xpB3Xu6OZb7svT5mw?view_as=subscriber"
+              )
+            }
+          />
+          <Button circular color="google plus" icon="google plus" onClick = {()=> alert("준비중입니다.")} />
+        </Grid>
+      </div>
     </div>
   );
 }
